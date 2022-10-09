d4b2b998f9fd1b24fe985ce8d1296f9cefdec758
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2022-10-09 15:06:06 +0300

    form a dictionary with the grouped data and return it

--- task_1/solution.py
+++ task_1/solution.py
@@ -6,12 +6,14 @@ PATH_TO_FILE = "data.csv"
 
 
 def group_by_country(file: str):
+    groupped_result = {}
     with open(file, "r") as f:
         read_data = reader(f)
         lst = sorted(tuple(reader(f))[1:], reverse=True)
         for country, groups in groupby(lst, lambda row: row[0]):
             names = [group[1] for group in groups]
-            print(country, names)
+            groupped_result[country] = {"people": names, "count": len(names)}
+        return groupped_result
 
         
         
