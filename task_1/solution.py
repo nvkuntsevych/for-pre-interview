1666877cd959a8e097586ce3ac8e890e8a58b87b
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2022-10-09 14:49:47 +0300

    read the data from data.csv

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,7 +1,17 @@
-def group_by_country():
-    pass
+import csv
+
+PATH_TO_FILE = "data.csv"
+
+
+def group_by_country(file: str):
+    with open(file, "r") as f:
+        read_data = csv.reader(f)
+        for s in read_data:
+            print(s)
+        
+        
 
 
 if __name__ == "__main__":
-    data = group_by_country()
+    data = group_by_country(file=PATH_TO_FILE)
     print(data)
