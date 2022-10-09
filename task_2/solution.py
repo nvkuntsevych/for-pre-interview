a3038ebb9dbf6de54d1480a9122ac372ffea6fce
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2022-10-09 16:47:45 +0300

    add `create_db` function

--- task_2/solution.py
+++ task_2/solution.py
@@ -5,10 +5,24 @@ PATH_TO_DB = "data.db"
 
 
 def create_connection(db: str) -> sq.Connection:
+    print("create_connection")
     con = sq.connect(db)
     return con
 
 
 def close_connection(con: sq.Connection) -> None:
+    print("close_connection")
     con.commit()
     con.close()
+
+
+def create_db(con: sq.Connection) -> None:
+    print("create_db")
+    cur = con.cursor()
+    query = ""
+    cur.execute(query)
+
+
+connection = create_connection(db=PATH_TO_DB)
+create_db(connection)
+close_connection(connection)
