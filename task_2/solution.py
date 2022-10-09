0fc01832fda712ba983cebd92876768847c63f22
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2022-10-09 21:30:34 +0300

    shorten long lines

--- task_2/solution.py
+++ task_2/solution.py
@@ -23,8 +23,11 @@ def create_table(con: sq.Connection) -> None:
     flag = check_if_connection(con)
     if flag:
         cur = con.cursor()
-        query = """CREATE TABLE IF NOT EXISTS tasks(task_id INTEGER PRIMARY KEY,
-                   task_name VARCHAR(50), task_isactive VARCHAR(1), task_closing_date DATE)"""
+        query = """CREATE TABLE IF NOT EXISTS tasks(
+                   task_id INTEGER PRIMARY KEY,
+                   task_name VARCHAR(50),
+                   task_isactive VARCHAR(1),
+                   task_closing_date DATE)"""
         cur.execute(query)
 
 
@@ -36,27 +39,32 @@ def add_task(con: sq.Connection, *task_names: tuple[str]) -> None:
     flag = check_if_connection(con) and check_if_not_empty(task_names)
     if flag:
         cur = con.cursor()
+        query = "INSERT INTO tasks (task_name, task_isactive)VALUES (?, 1)"
         for task_name in task_names:
-            cur.execute("INSERT INTO tasks (task_name, task_isactive)VALUES (?, 1)", (task_name, ))
+            cur.execute(query, (task_name, ))
         print("The tasks have been added")
 
 
 def remove_task(con: sq.Connection, *task_ids: tuple[str]) -> None:
     """Removes one of several tasks from db."""
-    flag = check_if_connection(con) and check_if_not_empty(task_ids) and check_if_all_tasks_exist(con, task_ids)
+    flag = check_if_connection(con) and (
+        check_if_not_empty(task_ids) and check_if_all_tasks_exist(con, task_ids) )
     if flag:
         cur = con.cursor()
+        query = "DELETE FROM tasks WHERE task_id == ?"
         for task_id in task_ids:
-            cur.execute("DELETE FROM tasks WHERE task_id == ?", (task_id, ))
+            cur.execute(query, (task_id, ))
         print("The tasks with", *task_ids, "ids have been removed")
 
 
 def mark_task(con: sq.Connection, task_id: str) -> None:
     """Marks the task with given id as done into db."""
-    flag = check_if_connection(con) and check_if_task_exist_and_isactive(con, task_id)
+    flag = check_if_connection(con) and (
+        check_if_task_exist_and_isactive(con, task_id) )
     if flag:
         cur = con.cursor()
-        query = "UPDATE tasks SET task_isactive = 0, task_closing_date = ? WHERE task_id == ?"
+        query = """UPDATE tasks SET task_isactive = 0, task_closing_date = ?
+                   WHERE task_id == ?"""
         cur.execute(query, (date.today(), task_id))
         print(f"The tasks with {task_id} id has been marked")
 
@@ -66,12 +74,14 @@ def list_active_task(con: sq.Connection) -> None:
     flag = check_if_connection(con)
     if flag:
         cur = con.cursor()
-        cur.execute("SELECT COUNT(task_name) FROM tasks WHERE task_isactive == 1")
+        query = "SELECT COUNT(task_name) FROM tasks WHERE task_isactive == 1"
+        cur.execute(query)
         active_task_numbers = cur.fetchone()[0]
         if active_task_numbers == 0:
             print("There are not active tasks")
         else:
-            cur.execute("SELECT task_id, task_name FROM tasks WHERE task_isactive == 1")
+            query = "SELECT task_id, task_name FROM tasks WHERE task_isactive == 1"
+            cur.execute(query)
             print("Your active tasks:\nid  task")
             for task in cur:
                 print(str(task[0]).ljust(3), task[1])
@@ -82,12 +92,15 @@ def list_statistic(con: sq.Connection) -> None:
     flag = check_if_connection(con)
     if flag:
         cur = con.cursor()
-        cur.execute("SELECT COUNT(task_name) FROM tasks WHERE task_isactive == 0")
+        query = "SELECT COUNT(task_name) FROM tasks WHERE task_isactive == 0"
+        cur.execute(query)
         not_active_task_numbers = cur.fetchone()[0]
         if not_active_task_numbers == 0:
             print("There are not completed tasks")
         else:
-            cur.execute("SELECT task_closing_date, COUNT(task_name) FROM tasks WHERE task_isactive == 0 GROUP BY task_closing_date")
+            query = """SELECT task_closing_date, COUNT(task_name) FROM tasks
+                       WHERE task_isactive == 0 GROUP BY task_closing_date"""
+            cur.execute(query)
             for task in cur:
                 print(f"{task[0]}: you've completed {task[1]} tasks!")
 
@@ -100,7 +113,7 @@ def check_if_connection(con: sq.Connection) -> bool:
     Returns True if it is or returns False and throws Exception otherwise.
     """
     if type(con) != sq.Connection:
-        raise ValueError("The first argument of function must be Connection object")
+        raise ValueError("The first argument must be Connection object")
         return False
     return True
 
@@ -122,8 +135,9 @@ def check_if_all_tasks_exist(con: sq.Connection, task_ids: tuple[str]) -> bool:
     Returns True if it is or returns False and throws Exception otherwise.
     """    
     cur = con.cursor()
+    query = "SELECT COUNT(task_name) FROM tasks WHERE task_id == ?"
     for task_id in task_ids:
-        cur.execute("SELECT COUNT(task_name) FROM tasks WHERE task_id == ?", (task_id, ))
+        cur.execute(query, (task_id, ))
         task_numbers = cur.fetchone()[0]
         if task_numbers == 0:
             raise ValueError("Command got the wrong ids")
@@ -137,10 +151,13 @@ def check_if_task_exist_and_isactive(con: sq.Connection, task_id: str) -> bool:
     Returns True if it is or returns False and throws Exception otherwise.
     """
     cur = con.cursor()
-    cur.execute("SELECT COUNT(task_name) FROM tasks WHERE task_id == ? AND task_isactive == 1", (task_id, ))
+    query = """SELECT COUNT(task_name) FROM tasks
+               WHERE task_id == ? AND task_isactive == 1"""
+    cur.execute(query, (task_id, ))
     task_numbers = cur.fetchone()[0]
     if task_numbers == 0:
-        raise ValueError(f"Task with {task_id} doesn't exist or this task is not active")
+        text = f"Task with {task_id} doesn't exist ort his task is not active"
+        raise ValueError(text)
         return False
     return True
 
@@ -154,7 +171,8 @@ def main() -> None:
     Reads and calls the necessary functions, catches the exception and
     print text of caught exception.
     """
-    print("Hello! This is TODO app. Enter 'help' to get information about using this app.")
+    print("Hello! This is TODO app.")
+    print("Enter 'help' to get information about using this app.")
     while True:
         entered_data = input("Insert command: ")
         if entered_data == "":
@@ -183,17 +201,18 @@ def main() -> None:
     print("TODO app has been finished.")
             
 
-commands_list = {"add": add_task, "remove": remove_task, "mark": mark_task, "list": list_active_task, "statistic": list_statistic}
+commands_list = {"add": add_task, "remove": remove_task, "mark": mark_task,
+                 "list": list_active_task, "statistic": list_statistic}
 
 help_string = """
 It is a simple TODO app with command line interface.
 
 This app supports the following commands:
 help - display this information;
-add <your_task> - add new task with description 'your_task'. You can enter multiple tasks 
-    separated by a comma;
-remove <task_id> - remove tasks with id 'task_id'. You can enter multiple
-    task ids separated by a comma;
+add <your_task> - add new task with description 'your_task'.
+    You can enter multiple tasks separated by a comma;
+remove <task_id> - remove tasks with id 'task_id'.
+    You can enter multiple task ids separated by a comma;
 list - display active tasks;
 mark <task_id> - mark task with id 'task_id' as done;
 statistic - display the number of comleted tasks grouped by date;
